de6b9a4f33111a0ac46241e1a748ba572aff93ee
Johan Rodríguez <jrodriguez0612@uta.edu.ec>
2026-02-24 20:12:36 -0500

    Se agregaron iconos de stack tecnologico en el hero

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, HeroComponent],
+  imports: [HeroComponent],
   templateUrl: './home.html',
   styleUrl: './home.sass',
 })
