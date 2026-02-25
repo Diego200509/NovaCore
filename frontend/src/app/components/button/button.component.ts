01837f742de6bae9328ad895b09d4abb92858647
Johan Rodríguez <jrodriguez0612@uta.edu.ec>
2026-02-24 19:41:30 -0500

    Se implementó el diseño basico de un hero

---
+++ frontend/src/app/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() variant: 'primary' | 'secondary' = 'primary';
+  @Input() disabled: boolean = false;
+}
